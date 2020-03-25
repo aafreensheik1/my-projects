47b542235a6e5f59b59d44b1eb9285d10521a3aa
aafreensheik1 <aafreen9495@gmail.com>
2020-03-26 01:50:28 +0530

    Added Functionality to the Calculator App

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 const Calculator = () => {
-    const cInput = ['C', '.', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', '', 0, '00', '='];
+    const cInput = ['C', 'CE', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '00', '.', '='];
+    const [userInput, setUserInput] = useState('');
+    const handleClick = (event) => {
+        switch (event.target.value) {
+            case 'CE':
+                setUserInput('');
+                break;
+            case 'C':
+                setUserInput((userInput).substring(0, (userInput).length - 1));
+                break;
+            case '=':
+                setUserInput(eval((userInput).replace('x', '*')));
+                break;
+            default:
+                setUserInput(userInput + event.target.value);
+                break;
+        }
+
+    }
     return (
         <div className='calculator-container'>
 
-            <input className='screen' />
+            <div className='screen'>{userInput}</div>
             <div className='button-container'>
-                {cInput.map(value => <button className='button'>{value}</button>)}
+                {cInput.map(value => <button className='button'
+                    onClick={(event) => handleClick(event)} value={value}>{value}</button>)}
             </div>
         </div>
     );
