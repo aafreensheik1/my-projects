a75aa14f7d5f7b7ca13975a12cee3010cb59d451
aafreensheik1 <aafreen9495@gmail.com>
2020-03-25 18:44:59 +0530

    Calculator First Commit

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './calculator.css';
 const Calculator = () => {
+    const cInput = ['C', '.', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', '', 0, '00', '='];
     return (
-        <div>
-            Calculator App
+        <div className='calculator-container'>
+
+            <input className='screen' />
+            <div className='button-container'>
+                {cInput.map(value => <button className='button'>{value}</button>)}
+            </div>
         </div>
     );
 }
